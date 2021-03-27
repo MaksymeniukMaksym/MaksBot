32b4f3fa234db286d33f8ccc6775b7a13b820bd1
Maksym <maksymmaksymeniuk@gmail.com>
2021-03-27 16:01:13 +0200

    first commit

---
+++ app/main.js
@@ -0,0 +1,31 @@
+const {
+    Telegraf
+} = require('telegraf')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const isLink = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/;
+
+const MaksDictionary = ["🔥", "топ"]
+
+
+
+const bot = new Telegraf("1102191595:AAEYFKpLkb_2VWacPOg_4glat6GLulYOBBM")
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+// bot.on('sticker', (ctx) => {
+//     ctx.reply('ok');
+//     console.log(ctx.update.message.from.username === 'ovojs')
+// })
+
+bot.hears(isLink, (ctx) => {
+    // if (ctx.update.message.from.username === 'ovojs') {
+    ctx.reply('🔥');
+    // }
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
